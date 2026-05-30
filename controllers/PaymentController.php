9f8b07d020d05ef76699129db5d4c2d5d5988b5b
ajf <doanthitramyt2004@gmail.com>
2026-05-30 20:01:04 +0700

    fix payment

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -5,8 +5,8 @@ class PaymentController
 {
     private $db;
     // --- CẤU HÌNH SEPAY ---
-    private $sepayToken = "CUBA1ZD6SPBMX9YBRLK8JJD40GJCS6RUF3WQKFYR2MON9VA10CZPSMQWNHKIZX7O";
-    private $accountNumber = "050134910132";
+    private $sepayToken = "S8KQDGER6VZO713BWXOAJYLEMPHMNVCDQVRZDBKKSYICN2TJ1XUWFGZZUIYV9GOP";
+    private $accountNumber = "00000419627";
 
     public function __construct()
     {
